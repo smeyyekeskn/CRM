061bd7d78f8d472b5045f507c5d581456e4e6e57
smeyyekeskn <smeyyekeskn@gmail.com>
2019-03-08 21:37:31 +0300

    Create Product Controller and Category Controller

---
+++ CRM.UI/Controllers/CategoryController.cs
@@ -0,0 +1,72 @@
+﻿using CRM.Model;
+using CRM.Service.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRM.UI.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService categoryService;
+        public CategoryController(ICategoryService categoryService)
+        {
+            this.categoryService = categoryService;
+
+        }
+        // GET: Category
+        public ActionResult Index()
+        {
+            var categories = categoryService.GetAll();
+            return View(categories);
+        }
+        public ActionResult Create()
+        {
+            var category = new Category();
+            return View(category);           
+        }
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                categoryService.Insert(category);
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+        public ActionResult Edit(Guid id)
+        {
+            var category = categoryService.Find(id);
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                var model = categoryService.Find(category.Id);
+                model.Name = category.Name;
+                model.Products = category.Products;
+                categoryService.Update(model);
+
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public ActionResult Delete(Guid id)
+        {
+            categoryService.Delete(id);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Details(Guid id)
+        {
+            return View(categoryService.Find(id));
+        }
+    }
+}
---
+++ CRM.UI/Controllers/ProductController.cs
@@ -0,0 +1,121 @@
+﻿using CRM.Model;
+using CRM.Service.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRM.UI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService productService;
+        private readonly ICategoryService categoryService;
+        public ProductController(IProductService productService)
+        {
+            this.productService = productService;
+        }
+        // GET: Product
+        public ActionResult Index()
+        {
+            var products = productService.GetAll();
+            return View(products);
+        }
+        public ActionResult Create()
+        {
+            var product = new Product();
+            ViewBag.Categories = new SelectList(categoryService.GetAll(), "Id", "Name", product.CategoryId);
+            return View(product);
+        }
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                productService.Insert(product);
+                return RedirectToAction("Index");
+            }
+            ViewBag.Categories = new SelectList(categoryService.GetAll(), "Id", "Name", product.CategoryId);
+            return View();
+        }
+
+        public ActionResult Edit(Guid id)
+        {
+            var product = productService.Find(id);
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+        
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                var model = productService.Find(product.Id);
+                model.Name = product.Name;
+                model.Offers = product.Offers;
+                model.Orders = product.Orders;
+                model.SerialNumber = product.SerialNumber;
+                model.Stock = product.Stock;
+                model.CategoryId = product.CategoryId;
+                model.BuyingPrice = product.BuyingPrice;
+                model.Customers = product.Customers;
+                model.Description = product.Description;
+
+                productService.Update(model);
+                return RedirectToAction("Index");
+            }
+            ViewBag.Categories = new SelectList(categoryService.GetAll(), "Id", "Name", product.CategoryId);
+            return View();
+        }
+
+        public ActionResult Delete(Guid id)
+        {
+            productService.Delete(id);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(Guid id)
+        {
+            return View(productService.Find(id));
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
