70b50a8b6ef7645861201979bb3bdb0762d597a6
smeyyekeskn <smeyyekeskn@gmail.com>
2019-03-13 15:07:27 +0300

    Created LogOff and Created Counters at Home->Index

--- CRM.UI/Controllers/HomeController.cs
+++ CRM.UI/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CRM.Service.Services.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,8 +10,23 @@ namespace CRM.UI.Controllers
     [Authorize]
     public class HomeController : Controller
     {
+        private readonly IOrderService orderService;
+        private readonly IEmployeeService employeeService;
+        private readonly ICustomerService customerService;
+        private readonly IProductService productService;
+        public HomeController(IOrderService orderService, IEmployeeService employeeService, ICustomerService customerService, IProductService productService)
+        {
+            this.orderService = orderService;
+            this.productService = productService;
+            this.customerService = customerService;
+            this.employeeService = employeeService;
+        }
         public ActionResult Index()
         {
+            ViewBag.OrderCount = orderService.GetAll().Count();
+            ViewBag.ProductCount = productService.GetAll().Sum(p=>p.Stock);
+            ViewBag.CustomerCount = customerService.GetAll().Count();
+            ViewBag.EmployeeCount = employeeService.GetAll().Count();
             return View();
         }
 
